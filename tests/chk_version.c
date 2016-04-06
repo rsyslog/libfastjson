9ea7e52041360c59d69c02cfb6215c5a010dc89e
Rainer Gerhards <rgerhards@adiscon.com>
2016-04-06 10:34:24 +0200

    testbench: add test for fjson_version()

---
+++ tests/chk_version.c
@@ -0,0 +1,17 @@
+#include "config.h"
+
+#include "json.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int __attribute__((unused)) argc, char __attribute__((unused)) **argv)
+{
+	if(strcmp(fjson_version(), VERSION)) {
+		fprintf(stderr, "ERROR: fjson_version reports '%s', VERSION is '%s'.\n",
+			fjson_version(), VERSION);
+		exit(1);
+	}
+	return 0;
+}
