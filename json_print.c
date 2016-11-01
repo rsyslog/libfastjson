c524f824bee20c300954a7d0ec561768bd3bf4d3
Emiel Bruijntjes <emiel.bruijntjes@copernica.com>
2016-11-01 15:54:06 +0100

    changed indentation to use tabs

--- json_print.c
+++ json_print.c
@@ -33,10 +33,10 @@
 /* Forward declaration of the write function */
 static size_t write(struct fjson_object *jso, int level, int flags, fjson_write_fn *func, void *ptr);
 
-/** 
+/**
  *  helper for accessing the optimized string data component in fjson_object
  *  @param  jso
- *  @return 
+ *  @return
  */
 static const char *get_string_component(struct fjson_object *jso)
 {
@@ -44,7 +44,7 @@ static const char *get_string_component(struct fjson_object *jso)
 		   jso->o.c_string.str.data : jso->o.c_string.str.ptr;
 }
 
-/** 
+/**
  *  string escaping
  *
  *  String escaping is a surprisingly performance intense operation.
@@ -118,9 +118,9 @@ static char needsEscape[256] = {
  */
 static size_t escape(const char *str, fjson_write_fn *func, void *ptr)
 {
-    int size;
-    char tempbuf[6];
-    size_t result = 0;
+	int size;
+	char tempbuf[6];
+	size_t result = 0;
 	const char *start_offset = str;
 	while(1) { /* broken below on 0-byte */
 		if(needsEscape[*((unsigned char*)str)]) {
@@ -136,8 +136,8 @@ static size_t escape(const char *str, fjson_write_fn *func, void *ptr)
 			case '\\':  result += func(ptr, "\\\\", 2); break;
 			case '/':   result += func(ptr, "\\/", 2); break;
 			default:
-                size = snprintf(tempbuf, sizeof(tempbuf), "\\u00%c%c", fjson_hex_chars[*str >> 4], fjson_hex_chars[*str & 0xf]);
-                result += func(ptr, tempbuf, size);
+				size = snprintf(tempbuf, sizeof(tempbuf), "\\u00%c%c", fjson_hex_chars[*str >> 4], fjson_hex_chars[*str & 0xf]);
+				result += func(ptr, tempbuf, size);
 				break;
 			}
 			start_offset = ++str;
@@ -145,30 +145,30 @@ static size_t escape(const char *str, fjson_write_fn *func, void *ptr)
 			++str;
 	}
 	if(str != start_offset) result += func(ptr, start_offset, str - start_offset);
-    return result;
+	return result;
 }
 
 /* add indentation */
 
 static size_t indent(int level, int flags, fjson_write_fn *func, void *ptr)
 {
-    // result variable, and loop counter
-    size_t result = 0;
-    int i;
+	// result variable, and loop counter
+	size_t result = 0;
+	int i;
 
-    // skip if pretty-printing is not needed
+	// skip if pretty-printing is not needed
 	if (!(flags & FJSON_TO_STRING_PRETTY)) return 0;
-    
-    // iterate to add the spaces
-    for (i = 0; i < level; ++i)
-    {
-        // write a tab or two spaces
-        if (flags & FJSON_TO_STRING_PRETTY_TAB) result += func(ptr, "\t", 1);
-        else result += func(ptr, "  ", 2);
-    }
-    
-    // done
-    return result;
+
+	// iterate to add the spaces
+	for (i = 0; i < level; ++i)
+	{
+		// write a tab or two spaces
+		if (flags & FJSON_TO_STRING_PRETTY_TAB) result += func(ptr, "\t", 1);
+		else result += func(ptr, "  ", 2);
+	}
+
+	// done
+	return result;
 }
 
 /* write a json object */
@@ -176,7 +176,7 @@ static size_t indent(int level, int flags, fjson_write_fn *func, void *ptr)
 static size_t write_object(struct fjson_object* jso, int level, int flags, fjson_write_fn *func, void *ptr)
 {
 	int had_children = 0;
-    size_t result = 0;
+	size_t result = 0;
 
 	result += func(ptr, "{" /*}*/, 1);
 	if (flags & FJSON_TO_STRING_PRETTY) result += func(ptr, "\n", 1);
@@ -213,16 +213,16 @@ static size_t write_object(struct fjson_object* jso, int level, int flags, fjson
 static size_t write_boolean(struct fjson_object* jso, fjson_write_fn *func, void *ptr)
 {
 	if (jso->o.c_boolean) return func(ptr, "true", 4);
-    else return func(ptr, "false", 5);
+	else return func(ptr, "false", 5);
 }
 
 /* write a json int */
 
 static size_t write_int(struct fjson_object* jso, fjson_write_fn *func, void *ptr)
 {
-    // temporary buffer
-    char tempbuffer[32];
-    size_t bytes = snprintf(tempbuffer, sizeof(tempbuffer), "%" PRId64, jso->o.c_int64);
+	// temporary buffer
+	char tempbuffer[32];
+	size_t bytes = snprintf(tempbuffer, sizeof(tempbuffer), "%" PRId64, jso->o.c_int64);
 	return func(ptr, tempbuffer, bytes);
 }
 
@@ -237,7 +237,7 @@ static size_t write_double(struct fjson_object* jso, int flags, fjson_write_fn *
 	 * ECMA 262 section 9.8.1 defines
 	 * how to handle these cases as strings
 	 */
-	if(isnan(jso->o.c_double)) 
+	if(isnan(jso->o.c_double))
 		size = snprintf(buf, sizeof(buf), "NaN");
 	else if(isinf(jso->o.c_double))
 		if(jso->o.c_double > 0)
@@ -279,7 +279,7 @@ static size_t write_array(struct fjson_object* jso, int level, int flags, fjson_
 {
 	int had_children = 0;
 	int ii;
-    size_t result = 0;
+	size_t result = 0;
 	result += func(ptr, "[", 1);
 	if (flags & FJSON_TO_STRING_PRETTY) result += func(ptr, "\n", 1);
 	for(ii=0; ii < fjson_object_array_length(jso); ii++)
@@ -309,34 +309,34 @@ static size_t write_array(struct fjson_object* jso, int level, int flags, fjson_
 
 static size_t write(struct fjson_object *jso, int level, int flags, fjson_write_fn *func, void *ptr)
 {
-    // if object is not set
+	// if object is not set
 	if (!jso) return func(ptr, "null", 4);
 
-    // check type
-    switch(jso->o_type) {
-    case fjson_type_null:       return func(ptr, "null", 4);
-    case fjson_type_boolean:    return write_boolean(jso, func, ptr);
-    case fjson_type_double:     return write_double(jso, flags, func, ptr);
-    case fjson_type_int:        return write_int(jso, func, ptr);
-    case fjson_type_object:     return write_object(jso, level, flags, func, ptr);
-    case fjson_type_array:      return write_array(jso, level, flags, func, ptr);
-    case fjson_type_string:     return write_string(jso, func, ptr);
-    default:                    return 0;
-    }
+	// check type
+	switch(jso->o_type) {
+	case fjson_type_null:       return func(ptr, "null", 4);
+	case fjson_type_boolean:    return write_boolean(jso, func, ptr);
+	case fjson_type_double:     return write_double(jso, flags, func, ptr);
+	case fjson_type_int:        return write_int(jso, func, ptr);
+	case fjson_type_object:     return write_object(jso, level, flags, func, ptr);
+	case fjson_type_array:      return write_array(jso, level, flags, func, ptr);
+	case fjson_type_string:     return write_string(jso, func, ptr);
+	default:                    return 0;
+	}
 }
 
 /* wrapper around fwrite() that has the same signature as fjson_write_fn */
 
 static size_t fwrite_wrapper(void *ptr, const char *buffer, size_t size)
 {
-    return fwrite(buffer, 1, size, ptr);
+	return fwrite(buffer, 1, size, ptr);
 }
 
 /* extended dump function to string */
 
 size_t fjson_object_dump_ext(struct fjson_object *jso, int flags, fjson_write_fn *func, void *ptr)
 {
-    // write the value
+	// write the value
 	return write(jso, 0, flags, func, ptr);
 }
 
@@ -344,7 +344,7 @@ size_t fjson_object_dump_ext(struct fjson_object *jso, int flags, fjson_write_fn
 
 size_t fjson_object_dump(struct fjson_object *jso, fjson_write_fn *func, void *ptr)
 {
-    // write the value
+	// write the value
 	return write(jso, 0, FJSON_TO_STRING_SPACED, func, ptr);
 }
 
@@ -352,13 +352,13 @@ size_t fjson_object_dump(struct fjson_object *jso, fjson_write_fn *func, void *p
 
 size_t fjson_object_write(struct fjson_object *obj, FILE *fp)
 {
-    return fjson_object_dump_ext(obj, FJSON_TO_STRING_SPACED, fwrite_wrapper, fp);
+	return fjson_object_dump_ext(obj, FJSON_TO_STRING_SPACED, fwrite_wrapper, fp);
 }
 
 /* write to a file with custom output flags */
 
 size_t fjson_object_write_ext(struct fjson_object *obj, int flags, FILE *fp)
 {
-    return fjson_object_dump_ext(obj, flags, fwrite_wrapper, fp);
+	return fjson_object_dump_ext(obj, flags, fwrite_wrapper, fp);
 }
 
